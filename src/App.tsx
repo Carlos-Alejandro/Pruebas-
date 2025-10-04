59c2c9c775276dfc1f9d0cf569e5c38420dea3a6
Brandocel <desarrollador@byhoka.com>
2025-10-04 13:34:57 -0500

    Circular

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center bg-white">
+      <h1 className="text-6xl font-extrabold tracking-tight text-gray-900">
+        circular
+      </h1>
+    </div>
+  );
+}
